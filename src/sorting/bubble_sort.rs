fc01673fc785d0f876d3f2ff5fffbbcac7b13ddf
Sachin Jakhar <mesachinjakhar@gmail.com>
2025-12-03 13:40:32 +0530

    optimization

--- src/sorting/bubble_sort.rs
+++ src/sorting/bubble_sort.rs
@@ -2,10 +2,16 @@ pub fn optimize_approach(arr: &mut Vec<i32>) -> &Vec<i32> {
     let n = arr.len() ;
 
     for i in 0..n  {
+        let mut is_swapping = false;
         for j in 0..n-i-1 {
             if arr[j] > arr[j + 1] {
                 arr.swap(j, j+1);
+                is_swapping = true;
             }
+        }
+
+        if !is_swapping {
+            return arr;
         }
     }
 
