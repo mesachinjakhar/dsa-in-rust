46866142a5b076b1e3ee88c25f4a28d513367e53
Sachin Jakhar <mesachinjakhar@gmail.com>
2025-12-04 21:42:48 +0530

    diagonal sum optimize

--- src/two_d_arrays/diagonal_sum.rs
+++ src/two_d_arrays/diagonal_sum.rs
@@ -27,3 +27,19 @@ pub fn brute_force_2(arr: Vec<Vec<i32>>, rows: i32, col: i32) -> i32 {
 
     max_sum
 }
+
+pub fn optimize_approach(arr: Vec<Vec<i32>>, rows: i32, col: i32) -> i32 {
+    let n = arr.len();
+    let mut max_sum = 0;
+
+    for i in 0..rows {
+        max_sum = max_sum + arr[i as usize][i as usize];
+
+        if i != n as i32 -1-i {
+            max_sum = max_sum + arr[i as usize][n - 1 - i];
+        }
+    }
+
+    max_sum
+
+}
