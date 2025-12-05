7a600cd98c33ff18ad70d63fc8b6b5060991248b
Sachin Jakhar <mesachinjakhar@gmail.com>
2025-12-05 19:27:51 +0530

    optimize approach

--- src/hashing/two_sum.rs
+++ src/hashing/two_sum.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 pub fn brute_force(arr: Vec<i32>, tar: i32) -> Option<(usize, usize)> {
 
     for i in 0..arr.len() {
@@ -27,6 +29,21 @@ pub fn better_approach(arr: &mut Vec<i32>, tar: i32) -> Option<(usize, usize)> {
         } else {
             st = st + 1;
         }
+    }
+
+    return None;
+}
+
+pub fn optimize_approach(arr: &mut Vec<i32>, tar: i32) -> Option<(usize, usize)> {
+    let mut m: HashMap<i32, usize> = HashMap::new();
+
+    for i in 0..arr.len() {
+        let first = arr[i];
+        let second = tar - first;
+        if let Some(v) = m.get(&second) {
+            return Some((i, *v));
+        }
+        m.insert(arr[i], i);
     }
 
     return None;
